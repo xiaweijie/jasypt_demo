8bd72bc7d09054103cf3948c7fb9eab21ec6b302
xiaweijie <1586308500@qq.com>
2019-09-03 21:17:21 +0800

    修改提交

---
+++ src/main/java/com/xwj/jasypt_demo/JasyptDemoApplication.java
@@ -0,0 +1,12 @@
+package com.xwj.jasypt_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasyptDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JasyptDemoApplication.class, args);
+    }
+
+}
